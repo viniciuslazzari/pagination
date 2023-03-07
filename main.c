d1140a81292466558160a58f94342fe11f9dbfa1
Vinícius Lazzari <42687487+viniciuslazzari@users.noreply.github.com>
2023-03-06 22:10:28 -0300

    ♻️ Refactor code

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
 #include <limits.h>
 #include <time.h>
 
-const int PAGE_SIZE = 4096;
-const int MAX_ARRAY_SIZE_PAGE = PAGE_SIZE / sizeof(double);
-
 int main(int argc, char *argv[]){
-    int i, j, cur, count, page_number;
-    int N = 1000, MAX_DELTA = 1000, MISS_EVERY = 1;
+    unsigned long long i, cur, page_number;
+    unsigned long long N = 1000;
+    int MISS_EVERY = 0;
+    int count, page_size, max_array_page_size;
     double *arr;
     double res;
     clock_t start, end;
     double cpu_time_used;
 
-    N = atoi(argv[1]);
-    MAX_DELTA = atoi(argv[2]);
-    MISS_EVERY = atoi(argv[3]);
+    N = atoll(argv[1]);
+    MISS_EVERY = atoi(argv[2]);
 
-    arr = (double*) calloc(N, sizeof(double));
+    page_size = getpagesize();
+    max_array_page_size = page_size / sizeof(arr);
+
+    arr = (double*) calloc(N, sizeof(arr));
 
     srand(time(0));
 
@@ -26,35 +29,31 @@ int main(int argc, char *argv[]){
         arr[i] = (double) rand() / INT_MAX;
     }
 
-    for (j = 0; j <= 1024; j++){
-        res = 0, cur = 0, count = 0, page_number = 0;
+    res = 0, cur = 0, count = 0, page_number = 0;
 
-        start = clock();
-        for (i = 0; i < N; i++){
-            if (j != 0 && count == j){
-                count = 0;
-                page_number++;
-            }
-
-            cur = page_number * MAX_ARRAY_SIZE_PAGE + count;
-
-            if (cur >= N){
-                page_number = 0;
-                cur = 0;
-            }
-
-            //printf("%d\n", cur);
-
-            res += arr[cur];
-
-            count++;
+    start = clock();
+    for (i = 0; i < N; ++i){
+        if (MISS_EVERY != 0 && count == MISS_EVERY){
+            count = 0;
+            ++page_number;
         }
-        end = clock();
 
-        cpu_time_used = ((double) end - start) / CLOCKS_PER_SEC; 
+        cur = page_number * max_array_page_size + count;
 
-        printf("miss every = %d, sec = %f, res = %f\n", j, cpu_time_used, res);
+        if (cur >= N){
+            page_number = 0;
+            cur = 0;
+        }
+
+        res += arr[cur];
+
+        ++count;
     }
+    end = clock();
+
+    cpu_time_used = ((double) end - start) / CLOCKS_PER_SEC;
+
+    printf("sec = %f, res = %f\n", cpu_time_used, res);
 
     return 0;
 }
